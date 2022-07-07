73b69f7e5829627af5f773be5437a5d78913a779
Muhammed Erdem <muhammed@erdem.dev>
2022-07-07 19:52:49 +0300

    product page created

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return (
+    <footer>
+      <h1>Footer</h1>
+    </footer>
+  );
+};
+export default Footer;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import styles from "./styles.module.scss";
+
+const Header = () => {
+  return (
+    <header className={styles.wrapper}>
+      <div className={styles.container}>
+        <Link href="/">
+          <a className={styles.logo}>TheGreatProduct</a>
+        </Link>
+        <div className={styles.nav}>
+          <Link href="/">
+            <a className={styles.navItem}>Home</a>
+          </Link>
+          <Link href="/">
+            <a className={styles.navItem}>Products</a>
+          </Link>
+          <Link href="/">
+            <a className={styles.navItem}>Pricing</a>
+          </Link>
+          <Link href="/">
+            <a className={styles.navItem}>About</a>
+          </Link>
+          <Link href="/">
+            <a className={styles.navItem}>Contact</a>
+          </Link>
+          <button className={styles.button}>Buy Now!</button>
+        </div>
+      </div>
+    </header>
+  );
+};
+export default Header;
