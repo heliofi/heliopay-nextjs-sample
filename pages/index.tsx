cf4f89665afe18dde38ba66b337203151f8a9a82
suren <suren@fifth-llc.com>
2023-03-10 16:56:35 +0400

    fix: fix favicons FEE-957

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,93 @@ import { Cluster } from "@solana/web3.js";
 import { HelioSDK } from "@heliofi/sdk";
 import { Paylink } from "@heliofi/common";
 
+type Favicon = {
+  rel: string;
+  href: string;
+  sizes?: string;
+  type?: string;
+};
+
+const favicons: Array<Favicon> = [
+  {
+    rel: 'apple-touch-icon',
+    sizes: '57x57',
+    href: '/favicon/apple-icon-57x57.png',
+  },
+  {
+    rel: 'apple-touch-icon',
+    sizes: '60x60',
+    href: '/favicon/apple-icon-60x60.png',
+  },
+  {
+    rel: 'apple-touch-icon',
+    sizes: '72x72',
+    href: '/favicon/apple-icon-72x72.png',
+  },
+  {
+    rel: 'apple-touch-icon',
+    sizes: '76x76',
+    href: '/favicon/apple-icon-76x76.png',
+  },
+  {
+    rel: 'apple-touch-icon',
+    sizes: '114x114',
+    href: '/favicon/apple-icon-114x114.png',
+  },
+  {
+    rel: 'apple-touch-icon',
+    sizes: '120x120',
+    href: '/favicon/apple-icon-120x120.png',
+  },
+  {
+    rel: 'apple-touch-icon',
+    sizes: '144x144',
+    href: '/favicon/apple-icon-144x144.png',
+  },
+  {
+    rel: 'apple-touch-icon',
+    sizes: '152x152',
+    href: '/favicon/apple-icon-152x152.png',
+  },
+  {
+    rel: 'apple-touch-icon',
+    sizes: '180x180',
+    href: '/favicon/apple-icon-180x180.png',
+  },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '192x192',
+    href: '/favicon/android-icon-192x192.png',
+  },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '32x32',
+    href: '/favicon/favicon-32x32.png',
+  },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '96x96',
+    href: '/favicon/favicon-96x96.png',
+  },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '16x16',
+    href: '/favicon/favicon-16x16.png',
+  },
+  {
+    rel: 'manifest',
+    href: '/favicon/manifest.json',
+  },
+  {
+    rel: 'shortcut icon',
+    href: '/favicon/favicon.ico',
+  },
+];
+
 const Home: NextPage = () => {
   const [paymentRequestId, setPaymentRequestId] = useState<string>(
     "63c5b1a765f452f94a1e5ade"
@@ -38,7 +125,9 @@ const Home: NextPage = () => {
       <Head>
         <title>Helio Embed Sample App</title>
         <meta name="description" content="Helio Embed Sample App" />
-        <link rel="icon" href="/favicon.ico" />
+        {favicons.map((linkProps) => (
+          <link key={linkProps.href} {...linkProps} />
+        ))}
       </Head>
 
       <main className={styles.main}>
